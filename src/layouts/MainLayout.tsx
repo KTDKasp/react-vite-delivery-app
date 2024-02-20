222e1d9621f6e91710301011d7bcdf7481a0e4a9
KTDKasp <churroskasp@gmail.com>
2024-02-20 19:40:09 +0300

    Выполнена верстка левой боковой панели

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import { Button } from '../components/Button';
 
 import './MainLayout.css';
@@ -7,22 +7,27 @@ import './MainLayout.css';
 export const MainLayout: React.FC = () => {
   return (
     <div className="layout">
-      <div className="layout__left">
-        <div>
-          <img src="./profile-pic-circle.png" alt="Фото профиля" />
+      <div className="layout__sidebar">
+        <div className='sidebar__user'>
+          <img className='sidebar__pic' src="./profile-pic-circle.png" alt="Фото профиля" />
+          <h2 className='sidebar__username'>Джек Лондон</h2>
+          <p className='sidebar__email'>london@ya.ru</p>
         </div>
-        <h2>Джек Лондон</h2>
-        <p>london@ya.ru</p>
-        <div>
-          <Link to="/">Click to menu</Link>
-          <Link to="/cart">Click to cart</Link>
+        <div className='sidebar__nav'>
+          <NavLink to="/" className='sidebar__link'>
+            <img src="./menu-icon.svg" alt="Иконка меню" />
+            Меню
+          </NavLink>
+          <NavLink to="/cart" className='sidebar__link'>
+            <img src="./cart-icon.svg" alt="Иконка корзины" />
+            Корзина
+          </NavLink>
         </div>
         <Button className="exit-button">
           <img src="./on-off.svg" alt="Выход" />
           <span>Выйти</span>
         </Button>
       </div>
-      <div className="layout__border-line"></div>
       <div>
         <Outlet />
       </div>
