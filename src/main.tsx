5e33e0068e22d3de3f94c78541d02fd0a8e23fde
KTDKasp <churroskasp@gmail.com>
2024-02-24 16:57:14 +0300

    Верстка регистрации и пустой redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,39 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import axios from 'axios';
 import { RouterProvider, createBrowserRouter, defer } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import { MainLayout } from './layouts/MainLayout';
-import { Cart } from './pages/Cart';
-import { Product } from './pages/Product';
 import { PREFIX } from './helpers/API.ts';
 import { AuthLayout } from './layouts/AuthLayout';
-import Register from './pages/Register';
+import { MainLayout } from './layouts/MainLayout';
+import { Cart } from './pages/Cart';
 import Login from './pages/Login';
+import { Product } from './pages/Product';
+import Register from './pages/Register';
+import { RequireAuth } from './helpers/RequireAuth.tsx';
+import { store } from './store/store.ts';
 
 import './index.css';
-import { RequireAuth } from './helpers/RequireAuth.tsx';
 
 const Menu = React.lazy(() => import('./pages/Menu'));
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <RequireAuth><MainLayout /></RequireAuth>,
+    element: (
+      <RequireAuth>
+        <MainLayout />
+      </RequireAuth>
+    ),
     children: [
       {
         index: true,
-        element: <React.Suspense fallback={<>Загрузка...</>}><Menu /></React.Suspense>
+        element: (
+          <React.Suspense fallback={<>Загрузка...</>}>
+            <Menu />
+          </React.Suspense>
+        )
       },
       {
         path: 'cart',
@@ -32,14 +42,19 @@ const router = createBrowserRouter([
       {
         path: 'product/:id',
         element: <Product />,
-        errorElement: <h2 style={{ 'padding': '40px 60px' }}>Ошибка загрузки продукта...</h2>,
+        errorElement: (
+          <h2 style={{ padding: '40px 60px' }}>Ошибка загрузки продукта...</h2>
+        ),
         loader: async ({ params }) => {
           return defer({
-            data: axios.get(`${PREFIX}/products/${params.id}`).then(data => data).catch(err => err)
+            data: axios
+              .get(`${PREFIX}/products/${params.id}`)
+              .then((data) => data)
+              .catch((err) => err)
           });
           // const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
           // return data;
-        } 
+        }
       }
     ]
   },
@@ -65,6 +80,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,15 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { Headling } from '../../components/Headling';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 
 import './Register.css';
 
 const Register: React.FC = () => {
-	return (
-		<>
-			<Headling>Регистрация</Headling>
-			<div>Register</div>
-		</>
-	);
+  return (
+    <>
+      <Headling>Регистрация</Headling>
+      <form className="login__form">
+        <label className="login__label" htmlFor="email">
+          Ваш email
+        </label>
+        <Input type="email" placeholder="Email" id="email" name="email" />
+        <label className="login__label" htmlFor="password">
+          Ваш пароль
+        </label>
+        <Input
+          type="password"
+          placeholder="Пароль"
+          id="password"
+          name="password"
+        />
+        <label className="login__label" htmlFor="password">
+          Ваше имя
+        </label>
+        <Input
+          type="password"
+          placeholder="Имя"
+          id="password"
+          name="password"
+        />
+        <div className="login__button">
+          <Button type="submit" appearance="big">
+            Зарегистрироваться
+          </Button>
+        </div>
+      </form>
+      <div className="sign-up__ask">
+        <p>Есть акканут?</p>
+        <Link to="/auth/login" className="sign-up__link">
+          Войти
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Register;
